466493ab36430ffa3cc3674cfff9b601fa3eefa4
jhertz <jh.furtado@camus.fct.unl.pt>
2022-10-19 00:56:43 +0100

    made some changes and tried to make this thing more general

--- src/distanceMatrix.py
+++ src/distanceMatrix.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 from random import randrange, random
-from math import exp 
+from math import exp
 
 FILE_NAME = '../Distancias.txt'
 
@@ -136,7 +136,6 @@ def initialState(cities):
     return initial
 
 
-
 def randomNeighbour(path):
     '''basically it chooses two cities and switches them'''
 
@@ -166,72 +165,125 @@ def get_inital_temp(m, cities):
 
     return 2*d_max  - 2*d_min
 
+# an object with all that is needed to run the search
+def searchSolution(cfg):
 
-def searchSolution(cities):
-
-    m = readDistanceMatrix(FILE_NAME)
-    #constants to take from user
-    max_tot_iter = 1e3
-    max_n_iter = len(cities) 
-    init_temp = get_inital_temp(m, cities)# calculate the value later
-    alpha = 0.84 #(0.99 - 0.8) + 0.8
-    max_accepted = max_n_iter * 0.7
-    accept_factor = 0.1
-
-    pars = {'max_n_iter':max_n_iter,
-            'init_temp': init_temp,
-            'alpha': alpha,
-            'max_accepted': max_accepted,
-            'accept_factor': accept_factor,
-    }
-
+    # constants to take from user
+    # alpha =  0.89 
+    # init_temp = get_inital_temp(m, cities)# calculate the value later
+    # accept_factor = 0.001
+    # max_accepted = max_n_iter * 0.7
     # other variables
-    current = initialState(cities)
-    temperature = init_temp
+
+    #m = readDistanceMatrix(FILE_NAME)
+
+
+    cost_func = cfg.cost_func 
+    neighbour = cfg.neighbour 
+
+    current = cfg.init_sol #initialState(cities)
+    temperature = cfg.init_temp 
+    n_iter  = cfg.n_iter
+
     best = current
-    accepted = 0
     tot_iter = 0 
 
     while temperature:
-        n_iter = 0
-        for i in range(max_n_iter):
-            n_iter += 1
-            next = randomNeighbour(current)
-            diff = pathCost(m, current) - pathCost(m, next)
+        num_last_iter = 0
+        accepted = 0
+        for i in range(n_iter):
+            num_last_iter += 1
+            next = neighbour(current)
+            diff = cost_func(current) - cost_func(next)
             if diff > 0:
                 current = next
                 accepted += 1
-
+                
+                # what about this ???
+                # I think just have to drop this thing down :)
+                '''
                 if accepted > max_accepted:
                     break
-
+                '''
             else:
                 prob = exp(diff/temperature)
                 current = next if prob >= random() else current
                 n_iter += 1
 
-            best = next if pathCost(m, best) - pathCost(m, next) > 0 else best
+            best = next if cost_func(best) - cost_func(next) > 0 else best
 
-        temperature *= alpha # decreate the temperature
-        tot_iter += n_iter
+        tot_iter += num_last_iter
 
-        if accepted / n_iter < accept_factor or tot_iter >= max_tot_iter:
+        # updates some stuffs :)
+        temperature = cfg.lower_temp(temperature) 
+        n_iter = cfg.var_n_iter(n_iter)
+
+        if cfg.terminal_test([tot_iter, accepted, num_last_iter]):
             return best
 
     return best
 
-
-m = readDistanceMatrix(FILE_NAME)
-
-path = 'Atroeira, Douro, Pinhal, Teixoso, Ulgueira, Vilar'.split(', ')
-print(path)
-print(randomNeighbour(path))
+# IDEA BUT PROBABLY BAD
 '''
-sol = searchSolution(path.split(', '))
+class Settings:
+    def __init__(self, cities):
+        pass
 
-print(sol, ' cost: ', pathCost(m, sol))
+    def set_max_terminal(self, max):
+        self.terminal_test = lambda info : info[0] >= max
+
+    def set_accept_terminal(self, accept_factor):
+        self.terminal_test = lambda info : info[1]/info[2] < accept_factor
+
+    def set_init_temp(self, init_temp):
+        self.init_temp = init_temp
+    
+    def set_linear_var_iter(self, factor):
+        self.var_n_iter = lambda old_t : old_t * factor
+
+    def set_const_var_iter(self, const):
+        self.var_n_iter = lambda old_t : const 
+
+    def set_geo_lower_temp(self, alpha):
+        self.lower_temp = lambda temp : temp * alpha
+
+    def set_arith_lower_temp(self, alpha):
+        self.lower_temp = lambda temp : temp * alpha
 '''
 
-# 1436.0
+class Configs:
+    pass
 
+
+if __name__ == '__main__':
+
+    m = readDistanceMatrix(FILE_NAME)
+
+    #path = 'Belmar, Cerdeira, Douro, Encosta, Freita, Gonta, Horta, Infantado, Lourel, Monte, Nelas, Oura, Pinhal, Quebrada, Roseiral, Serra, Teixoso, Ulgueira'
+    #path = 'Atroeira, Douro, Pinhal, Teixoso, Ulgueira, Vilar'
+    #cities = path.split(', ')
+
+    cities = m[0]
+
+    config = Configs()
+
+    # CHANGE THE FOLLOWING FOR THE USER INPUT :)
+    # we gotta change both the strategy and 
+    # the parameters for each one of them
+    config.init_temp = get_inital_temp(m, cities)
+    config.n_iter = len(cities)
+    config.init_sol = initialState(cities)
+    # set of functions that are needed :)
+    config.var_n_iter = lambda _ : config.n_iter
+    config.lower_temp  = lambda t : t * 0.84
+    config.terminal_test = lambda info : info[0] >= 2e3
+
+    # this are all up to the problem so don't touch :)
+    # THINK ABOUT THIS...
+    config.cost_func = lambda city : pathCost(m, city) 
+    config.neighbour =  lambda city : randomNeighbour(city)
+
+    sol = searchSolution(config)
+
+    print(sol, ' cost: ', pathCost(m, sol))
 
