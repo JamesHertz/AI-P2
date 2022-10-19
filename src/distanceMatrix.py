5e4f3cc5aea1922120cfe619b5a8ad5f8091d982
jhertz <jh.furtado@camus.fct.unl.pt>
2022-10-19 14:31:31 +0100

    used decorator pattern to solve the big problem and added a problem class

--- src/distanceMatrix.py
+++ src/distanceMatrix.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+from abc import abstractmethod
+from lib2to3.pytree import Base
 from random import randrange, random
 from math import exp
 
@@ -113,60 +115,181 @@ def getInitials(cityList):
 #                   Our solution starts from here
 # -----------------------------------------------------------------
 
-# [1, 2, 3] => 1-2, 2-3, 3-1
-def pathCost(m, path):
-    totcost = distance(m,path[0], path[-1])
-    prev = path[0]
-    for curr in path[1:]:
-        totcost += distance(m, prev, curr)
-        prev = curr
+class Problem:
 
-    return totcost
+    @abstractmethod
+    def init_sol(self):
+        pass
 
+    # think about this
+    @abstractmethod
+    def calc_init_temp(self):
+        pass
 
-# problem formulation
-def initialState(cities):
-    aux = cities[:]
-    initial = []
-    while len(aux) != 0:
-        idx = randrange(len(aux))
-        initial.append(aux[idx])
-        del aux[idx]
+    @abstractmethod
+    def cost_func(self, node):
+        pass
 
-    return initial
+    @abstractmethod
+    def neighbour(self, node):
+        pass
 
 
-def randomNeighbour(path):
-    '''basically it chooses two cities and switches them'''
 
-    def randomIdx():
-        return randrange(len(path))
+class TravSalemanProblem(Problem):
 
-    idx1 = randomIdx()
-    idx2 = randomIdx()
+    def __init__(self, cities, dm):
+        self.dm  = createReducedMatrix(dm, cities)
+        self.cities = cities # think about this
 
-    while abs(idx1 - idx2) <= 1: # while the idxs are adjacent
-        idx2 = randomIdx() 
+    def cost_func(self, node):
+        totcost = distance(self.dm,node[0], node[-1])
 
-    i = min(idx1, idx2)
-    j = max(idx1, idx2)
+        prev = node[0]
+        for curr in node[1:]:
+            totcost += distance(self.dm, prev, curr)
+            prev = curr
 
-    return path[:i + 1] + path[j: i: -1] + path[j+1:]
+        return totcost 
+
+    def neighbour(self, node):
+
+        def randomIdx():
+            return randrange(len(node))
+
+        idx1 = randomIdx()
+        idx2 = randomIdx()
+
+        while abs(idx1 - idx2) <= 1: # while the idxs are adjacent
+            idx2 = randomIdx() 
+
+        i = min(idx1, idx2)
+        j = max(idx1, idx2)
+
+        return node[:i + 1] + node[j: i: -1] + node[j+1:]
+
+    # think about this :)
+    def init_sol(self):
+        aux = self.cities[:]
+        initial = []
+        while len(aux) != 0:
+            idx = randrange(len(aux))
+            initial.append(aux[idx])
+            del aux[idx]
+
+        return initial
+
+    def calc_init_temp(self):
+        distances = self.dm[1]
+        d_max = distances[0][0]
+        d_min = distances[0][0]
+        for r in distances:
+            for d in r:
+                d_min = min(d, d_min)
+                d_max = max(d, d_max)
+
+        return 2*d_max  - 2*d_min
+
+# I used decorator a patter that we have studied at the class
+class Configs:
+
+    def __init__(self, n_iter, init_temp):
+        self.n_iter = n_iter
+        self.init_temp = init_temp
+
+    def get_init_temp(self):
+        return self.init_temp
+
+    def get_n_iter(self):
+        return self.n_iter
+
+    def lower_temp(self, temp):
+        pass
+
+    def var_n_iter(self, n_iter):
+        pass
+    
+    def terminal_test(self, info):
+        pass
+
+class CompositeConfig:
+    def __init__(self, base_cfg: Configs):
+        self.base = base_cfg
+
+    def get_init_temp(self):
+        return self.base.get_init_temp()
+
+    def get_n_iter(self):
+        return self.base.get_n_iter()
+
+    def lower_temp(self, temp):
+        return self.base.lower_temp(temp)
+
+    def var_n_iter(self, n_iter):
+        return self.base.var_n_iter(n_iter)
+    
+    def terminal_test(self, info):
+        return self.base.terminal_test(info)
 
 
-def get_inital_temp(m, cities):
-    [_, distances] = createReducedMatrix(m, cities)
-    d_max =  distances[0][0]
-    d_min = distances[0][0]
-    for r in distances:
-        for d in r:
-            d_min = min(d, d_min)
-            d_max = max(d, d_max)
+# lower_temp
+class ArithmeticLowerTemp(CompositeConfig):
 
-    return 2*d_max  - 2*d_min
+    def __init__(self, base_cfg, beta):
+        super().__init__(base_cfg)
+        self.beta = beta
+    
+    def lower_temp(self, temp):
+        return 0 # do the arithmetic thing
+
+
+class GeometricLowerTemp(CompositeConfig):
+    def __init__(self, base_cfg, alpha):
+        super().__init__(base_cfg)
+        self.alpha = alpha
+
+    def lower_temp(self, temp):
+        return self.alpha * temp
+
+# var_n_iter
+class ConstantNIterVar(CompositeConfig):
+    def __init__(self, base_cfg):
+        super().__init__(base_cfg)
+
+    def var_n_iter(self, n_iter):
+        return super().get_n_iter()
+
+
+class LinearNIterVar(CompositeConfig):
+    def __init__(self, base_cfg, factor):
+        super().__init__(base_cfg)
+        self.factor = factor
+
+    def var_n_iter(self, n_iter):
+        return n_iter * self.factor
+
+# terminal test
+class MaxIterTerminalTest(CompositeConfig):
+
+    def __init__(self, base_cfg, max_iter):
+        super().__init__(base_cfg)
+        self.max_iter = max_iter
+
+    def terminal_test(self, info):
+        return info[0] >= self.max_iter
+
+class AcceptFactorTerminalTest(CompositeConfig):
+
+    def __init__(self, base_cfg, accept_factor):
+        super().__init__(base_cfg)
+        self.accept_factor = accept_factor
+
+    def terminal_test(self, info):
+        return info[2]/info[1] < self.accept_factor
+
 
 # an object with all that is needed to run the search
-def searchSolution(cfg):
+def searchSolution(problem : Problem , cfg: Configs):
 
     # constants to take from user
     # alpha =  0.89 
@@ -177,13 +300,10 @@ def searchSolution(cfg):
 
     #m = readDistanceMatrix(FILE_NAME)
 
+    current = problem.init_sol()
 
-    cost_func = cfg.cost_func 
-    neighbour = cfg.neighbour 
-
-    current = cfg.init_sol #initialState(cities)
-    temperature = cfg.init_temp 
-    n_iter  = cfg.n_iter
+    temperature = cfg.get_init_temp()
+    n_iter  = cfg.get_n_iter()
 
     best = current
     tot_iter = 0 
@@ -193,8 +313,8 @@ def searchSolution(cfg):
         accepted = 0
         for i in range(n_iter):
             num_last_iter += 1
-            next = neighbour(current)
-            diff = cost_func(current) - cost_func(next)
+            next = problem.neighbour(current)
+            diff = problem.cost_func(current) - problem.cost_func(next)
             if diff > 0:
                 current = next
                 accepted += 1
@@ -210,7 +330,7 @@ def searchSolution(cfg):
                 current = next if prob >= random() else current
                 n_iter += 1
 
-            best = next if cost_func(best) - cost_func(next) > 0 else best
+            best = next if problem.cost_func(best) - problem.cost_func(next) > 0 else best
 
         tot_iter += num_last_iter
 
@@ -223,67 +343,29 @@ def searchSolution(cfg):
 
     return best
 
-# IDEA BUT PROBABLY BAD
-'''
-class Settings:
-    def __init__(self, cities):
-        pass
-
-    def set_max_terminal(self, max):
-        self.terminal_test = lambda info : info[0] >= max
-
-    def set_accept_terminal(self, accept_factor):
-        self.terminal_test = lambda info : info[1]/info[2] < accept_factor
-
-    def set_init_temp(self, init_temp):
-        self.init_temp = init_temp
-    
-    def set_linear_var_iter(self, factor):
-        self.var_n_iter = lambda old_t : old_t * factor
-
-    def set_const_var_iter(self, const):
-        self.var_n_iter = lambda old_t : const 
-
-    def set_geo_lower_temp(self, alpha):
-        self.lower_temp = lambda temp : temp * alpha
-
-    def set_arith_lower_temp(self, alpha):
-        self.lower_temp = lambda temp : temp * alpha
-'''
-
-class Configs:
-    pass
-
 
 if __name__ == '__main__':
 
-    m = readDistanceMatrix(FILE_NAME)
+    dm = readDistanceMatrix(FILE_NAME)
 
     #path = 'Belmar, Cerdeira, Douro, Encosta, Freita, Gonta, Horta, Infantado, Lourel, Monte, Nelas, Oura, Pinhal, Quebrada, Roseiral, Serra, Teixoso, Ulgueira'
     #path = 'Atroeira, Douro, Pinhal, Teixoso, Ulgueira, Vilar'
     #cities = path.split(', ')
 
-    cities = m[0]
+    cities = dm[0]
 
-    config = Configs()
 
-    # CHANGE THE FOLLOWING FOR THE USER INPUT :)
-    # we gotta change both the strategy and 
-    # the parameters for each one of them
-    config.init_temp = get_inital_temp(m, cities)
-    config.n_iter = len(cities)
-    config.init_sol = initialState(cities)
-    # set of functions that are needed :)
-    config.var_n_iter = lambda _ : config.n_iter
-    config.lower_temp  = lambda t : t * 0.84
-    config.terminal_test = lambda info : info[0] >= 2e3
+    problem = TravSalemanProblem(cities, dm)
 
-    # this are all up to the problem so don't touch :)
-    # THINK ABOUT THIS...
-    config.cost_func = lambda city : pathCost(m, city) 
-    config.neighbour =  lambda city : randomNeighbour(city)
+    config = Configs(len(cities), problem.calc_init_temp())
 
-    sol = searchSolution(config)
+    config = MaxIterTerminalTest(config, 2e3)
 
-    print(sol, ' cost: ', pathCost(m, sol))
+    config = GeometricLowerTemp(config, 0.84)
+
+    config = ConstantNIterVar(config)
+
+    sol = searchSolution(problem, config)
+
+    print(sol, 'cost:', problem.cost_func(sol))
 
